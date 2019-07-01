511241cd10b72f8bc4bf9e7884e43f38645108e6
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-01 15:12:23 -0400

    login setup

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
     has_many :visits
     has_many :users, through: :visits
+    belongs_to :country
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :visits
     has_many :locations, through: :visits
+    validates :first_name, :last_name, :email, presence: true
 end
