fa1a08640fc39c11cc280ab8e23d6085a56c7967
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-01 16:28:44 -0400

    added labels

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,5 @@ class Location < ApplicationRecord
     has_many :visits
     has_many :users, through: :visits
     belongs_to :country
-<<<<<<< HEAD
-
     validates :name, :description, :country_id, presence:true
-=======
->>>>>>> 511241cd10b72f8bc4bf9e7884e43f38645108e6
 end
